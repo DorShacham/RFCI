3406f5a1ddd0c4bb46f4bdbf4f687b37708c8eb7
DorShacham <dor.shacham@gmail.com>
2024-10-15 11:56:57 +0300

    configs

--- slurm_script/vqe_2.sh
+++ slurm_script/vqe_2.sh
@@ -2,10 +2,10 @@
 
 ##Resource Request
 
-#SBATCH --job-name vqe-1
+#SBATCH --job-name vqe-2
 #SBATCH --mail-user=dor-hay.sha@campus.technion.ac.il
 #SBATCH --mail-type=ALL           # Valid values are NONE, BEGIN, END, FAIL, REQUEUE, ALL
-#SBATCH --output /home/dor-hay.sha/master/RFCI/slurm_script/output/vqe-1-result-%j.out   ## filename of the output; the %j is equivalent to jobID; default is slurm-[jobID].out
+#SBATCH --output /home/dor-hay.sha/master/RFCI/slurm_script/output/vqe-2-result-%j.out   ## filename of the output; the %j is equivalent to jobID; default is slurm-[jobID].out
 #SBATCH --ntasks=1  ## number of tasks (analyses) to run
 #SBATCH -c 20
 #SBATCH --time=0-10:10:00  ## time for analysis (day-hour:min:sec)
@@ -18,5 +18,5 @@ conda activate rfci-env
 
 ## Run the script
 nvidia-smi
-python vqe_simulation.py --config_path ./configs/config_1.yaml
+python vqe_simulation.py --config_path ./configs/config_2.yaml
 echo Done
