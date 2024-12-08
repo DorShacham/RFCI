bfa2e8e9ad366ea06ca96e4afe383880ce831c5a
DorShacham <dor.shacham@gmail.com>
2024-12-08 10:56:18 +0200

    exact diagnolization (3,6) result

--- slurm_script/tmp.sh
+++ slurm_script/tmp.sh
@@ -2,13 +2,13 @@
 
 ##Resource Request
 
-#SBATCH --job-name spectral_flow
+#SBATCH --job-name diagnolization
 #SBATCH --mail-user=dor-hay.sha@campus.technion.ac.il
 #SBATCH --mail-type=ALL           # Valid values are NONE, BEGIN, END, FAIL, REQUEUE, ALL
 #SBATCH --output /home/dor-hay.sha/master/RFCI/slurm_script/output/spectral_flow-result-%j.out   ## filename of the output; the %j is equivalent to jobID; default is slurm-[jobID].out
 #SBATCH --ntasks=1  ## number of tasks (analyses) to run
-#SBATCH -c 7
-#SBATCH --time=0-4:10:00  ## time for analysis (day-hour:min:sec)
+#SBATCH -c 20
+#SBATCH --time=2-20:10:00  ## time for analysis (day-hour:min:sec)
 
 ##Load the CUDA module
 module load cuda
@@ -18,5 +18,5 @@ conda activate rfci-env
 
 ## Run the script
 nvidia-smi
-python playground.py
+python tmp.py
 echo Done
