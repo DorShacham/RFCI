888194f1e1018979bcecd2768b3e9f5f266d69f9
Dor Shacham <dor.shacham@gmail.com>
2025-02-10 15:53:08 +0200

    .

--- condor_scripts/build_matrix.sh
+++ condor_scripts/build_matrix.sh
@@ -3,7 +3,7 @@
 source /Local/ph_lindner/anaconda3/bin/activate rfci-env
 
 # Run the Python script
-python ~/scripts/RFCI/RFCI/build_matrix.py --matrix_type H -Nx 3 -Ny 3 --cpu 6 \ 
+python ~/scripts/RFCI/RFCI/build_matrix.py --matrix_type H -Nx 3 -Ny 3 -cpu 6 \ 
 --save_path /storage/ph_lindner/dorsh/RFCI/data/matrix/spectral_flow \
 --name $0 --phi $1
 
