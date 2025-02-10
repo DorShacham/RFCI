7c6b27f2302a72d6a58ec3053a7473a4d22db67f
Dor Shacham <dor.shacham@gmail.com>
2025-02-10 19:35:43 +0200

    .

--- condor_scripts/build_matrix.sh
+++ condor_scripts/build_matrix.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-TARGET_FILE="/storage/ph_lindner/dorsh/RFCI/data/matrix/spectral_flow/H_Nx-2_Ny-6_$1.npz"
+TARGET_FILE="/storage/ph_lindner/dorsh/RFCI/data/matrix/spectral_flow/H_Nx-3_Ny-6_$1.npz"
 
 # Check if the file exists
 if [ -f "$TARGET_FILE" ]; then
