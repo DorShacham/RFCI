eb5e3ea366842fd2562e5db754e46a92027619df
Dor Shacham <dor.shacham@gmail.com>
2025-02-16 15:19:12 +0200

    .

--- condor_scripts/run_tmp.sh
+++ condor_scripts/run_tmp.sh
@@ -3,6 +3,7 @@
 source /Local/ph_lindner/anaconda3/bin/activate rfci-env
 
 # Run the Python script
+python ~/scripts/RFCI/RFCI/python build_matrix.py -Nx 3 -Ny 6 -cpu 10
 python ~/scripts/RFCI/RFCI/shared_tmp.py
 
 conda deactivate
