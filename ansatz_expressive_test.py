6103e439d3df0367b277749532ad9f923d1a9324
Dor Shacham <dor.shacham@gmail.com>
2025-02-25 14:13:30 +0200

    .

--- ansatz_expressive_test.py
+++ ansatz_expressive_test.py
@@ -21,8 +21,11 @@ else:
     print("Operating system not recognized. No changes applied.")
 
 
-
 import jax
+from jax import config
+# Enable 64-bit computation
+config.update("jax_enable_x64", True)
+import jax.numpy as jnp
 import yaml
 from exact_diagnolization import *
 import jax_simulation
@@ -47,24 +50,24 @@ q = config['data']['q']
 saving_dir = str(f"results/vqe_simulation/jax/ansatz_expressive_test/Nx-{Nx}_Ny-{Ny}_p-{p}_q-{q}")
 layer_numer_range = range(1,5)
 
-# overlap_array = []
-# for layer_numer in layer_numer_range:
-#     id = wandb.util.generate_id()
-#     config['data']['id'] = id
+overlap_array = []
+for layer_numer in layer_numer_range:
+    id = wandb.util.generate_id()
+    config['data']['id'] = id
 
-#     wandb.init(
-#         # set the wandb project where this run will be logged
-#         project="RFCI-vqe",
-#         name= f"Nx-{Nx}_Ny-{Ny}_p-{p}_q-{q}/expressive_test/{id}",
-#         id = id,
-#         # track hyperparameters and run metadata
-#         config= config
-#         )
-#     saving_path = saving_dir +  str(f"/ln-{layer_numer}_flux")
-#     config_list[0]['flux_gate_true'] = True
-#     config_list[0]['layer_numer'] = layer_numer
-#     res = jax_simulation.vqe_simulation(Nx = Nx, Ny = Ny, config_list = config_list, n = n, p=p, q=q, pre_ansatz = None,saveto = saving_path, log = True)
-#     overlap_array.append(np.abs(res.fun))
+    wandb.init(
+        # set the wandb project where this run will be logged
+        project="RFCI-vqe",
+        name= f"Nx-{Nx}_Ny-{Ny}_p-{p}_q-{q}/expressive_test/{id}",
+        id = id,
+        # track hyperparameters and run metadata
+        config= config
+        )
+    saving_path = saving_dir +  str(f"/ln-{layer_numer}_flux")
+    config_list[0]['flux_gate_true'] = True
+    config_list[0]['layer_numer'] = layer_numer
+    res = jax_simulation.vqe_simulation(Nx = Nx, Ny = Ny, config_list = config_list, n = n, p=p, q=q, pre_ansatz = None,saveto = saving_path, log = True)
+    overlap_array.append(np.abs(res.fun))
 
 overlap_array_no_flux = []
 for layer_numer in layer_numer_range:
