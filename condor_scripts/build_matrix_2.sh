3e5a8fe5ec4861aa8bf4cf103798a6151af5b739
Dor Shacham <dor.shacham@gmail.com>
2025-02-27 18:05:03 +0200

    .

---
+++ condor_scripts/build_matrix_2.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+TARGET_FILE="/storage/ph_lindner/dorsh/RFCI/data/matrix/spectral_flow/H_Nx-2_Ny-6_$1.npz"
+
+# Check if the file exists
+if [ -f "$TARGET_FILE" ]; then
+    echo "$TARGET_FILE exists."
+else
+    echo "$TARGET_FILE does not exist. Running script.py..."
+    
+    source /Local/ph_lindner/anaconda3/bin/activate rfci-env
+    # Run the Python script
+    python ~/scripts/RFCI/RFCI/build_matrix.py --matrix_type H -Nx 2 -Ny 6 -cpu 6 \
+    --save_path /storage/ph_lindner/dorsh/RFCI/data/matrix/spectral_flow \
+    --name $1 --phi $2
+
+    conda deactivate
+fi
+
+
+
