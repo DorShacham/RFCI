e4727dc719185d72995d1f7f4dc407bfcb7f8a37
Dor Shacham <dor.shacham@gmail.com>
2025-02-27 18:11:36 +0200

    .

--- condor_scripts/sp.sh
+++ condor_scripts/sp.sh
@@ -2,7 +2,7 @@
 
 source /Local/ph_lindner/anaconda3/bin/activate rfci-env
 # Run the Python script
-python ~/scripts/RFCI/RFCI/sp_condor.py -Nx 3 -Ny 6 -cpu 1
+python ~/scripts/RFCI/RFCI/sp_condor.py -Nx 2 -Ny 6 -cpu 1
 
 conda deactivate
 
