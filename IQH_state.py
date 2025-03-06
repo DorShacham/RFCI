db63df5904976436c75174395e2659215df9c2df
Dor Shacham <dor.shacham@gmail.com>
2025-03-06 08:19:42 +0200

    H building test

--- IQH_state.py
+++ IQH_state.py
@@ -196,12 +196,12 @@ class Multi_particle_state:
         return new_state
 
 # Create the single body Hamiltonian in real space
-def build_H(Nx = 2, Ny = 2, band_energy = 1, M = 0, phi = np.pi/4, phase_shift_x = 0, phase_shift_y = 0, element_cutoff= None):
+def build_H(Nx = 2, Ny = 2, band_energy = 1, M = 0, phi = np.pi/4, phase_shift_x = 0, phase_shift_y = 0, element_cutoff= None, flat_band = True):
 # parametrs of the model
     N = Nx * Ny
     # phi = np.pi/4
     t1 = 1
-    t2 = -(2-np.sqrt(2))/2 * t1
+    t2 = (2-np.sqrt(2))/2 * t1
     # t2 = t1 / np.sqrt(2)
 
     # Building the single particle hamiltonian (h2)
@@ -218,7 +218,8 @@ def build_H(Nx = 2, Ny = 2, band_energy = 1, M = 0, phi = np.pi/4, phase_shift_x
     def build_h2(kx, ky, band_energy):
         h11 = 2 * t2 * (np.cos(kx) - np.cos(ky)) + M
         h12 = t1 * np.exp(1j * phi) * (1 + np.exp(1j * (ky - kx))) + t1 * np.exp(-1j * phi) * (np.exp(1j * (ky)) + np.exp(1j * (- kx)))
-        h2 = np.array([[h11, h12], [np.conjugate(h12), -h11]])
+        # h2 = np.array([[h11, h12], [np.conjugate(h12), -h11]])
+        h2 = np.array([[h11, np.conjugate(h12)], [h12, -h11]])
         return h2
 
     H_k_list = []
@@ -226,11 +227,15 @@ def build_H(Nx = 2, Ny = 2, band_energy = 1, M = 0, phi = np.pi/4, phase_shift_x
     for kx in Kx:
         for ky in Ky:
             H_single_particle = build_h2(kx + phase_shift_x/Nx,ky + phase_shift_y/Ny, band_energy)
-            eig_val, eig_vec = np.linalg.eigh(H_single_particle)
-            h_flat = H_single_particle / np.abs(eig_val[0]) * band_energy + i * 1e-8  # flat band limit + small disperssion for numerical stabilty
-            H_k_list.append(h_flat)
-            # H_k_list.append(H_single_particle)
+            if flat_band:
+                eig_val, eig_vec = np.linalg.eigh(H_single_particle)
+                h_flat = H_single_particle / np.abs(eig_val[0]) * band_energy + i * 1e-8  # flat band limit + small disperssion for numerical stabilty
+                H_k_list.append(h_flat)
+            else:
+                H_k_list.append(H_single_particle+ i * 1e-8)   #  small disperssion for numerical stabilty
             i += 1
+            
+            
             
     # creaing a block diagonal H_k matrix and dft to real space
 
@@ -239,7 +244,7 @@ def build_H(Nx = 2, Ny = 2, band_energy = 1, M = 0, phi = np.pi/4, phase_shift_x
     # dft matrix as a tensor protuct of dft in x and y axis and idenity in the sublattice
     dft_matrix = np.kron(dft(Nx, scale='sqrtn'),(np.kron(dft(Ny, scale='sqrtn'),np.eye(2))))
     # dft_matrix = np.kron(lattice_dft(Nx),(np.kron(lattice_dft(Ny),np.eye(2))))
-    H_real_space = dft_matrix @ H_k @ dft_matrix.T.conjugate()
+    H_real_space = -dft_matrix @ H_k @ dft_matrix.T.conjugate()
 
     if element_cutoff is not None:
         H_real_space[np.abs(H_real_space) < element_cutoff] = 0
