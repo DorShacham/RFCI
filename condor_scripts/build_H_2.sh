a05c4e4a3906e8d7aabf9bbf3af11cd0a74ceff0
Dor Shacham <dor.shacham@gmail.com>
2025-03-13 11:44:50 +0200

    .

--- condor_scripts/build_H_2.sh
+++ condor_scripts/build_H_2.sh
@@ -3,7 +3,7 @@
 source /Local/ph_lindner/anaconda3/bin/activate rfci-env
 
 # Run the Python script
-python ~/scripts/RFCI/RFCI/build_matrix.py -Nx 3 -Ny 5 -cpu 10
+python ~/scripts/RFCI/RFCI/build_matrix.py -Nx 5 -Ny 3 -cpu 10
 
 conda deactivate
 
--- condor_scripts/diag_sp_matrix_2.sh
+++ condor_scripts/diag_sp_matrix_2.sh
@@ -2,7 +2,7 @@
 
 source /Local/ph_lindner/anaconda3/bin/activate rfci-env
 # Run the Python script
-python ~/scripts/RFCI/RFCI/diagnolization_condor.py -Nx 3 -Ny 5 -cpu 6 \
+python ~/scripts/RFCI/RFCI/diagnolization_condor.py -Nx 5 -Ny 3 -cpu 6 \
 --matrix_index $1
 
 conda deactivate
