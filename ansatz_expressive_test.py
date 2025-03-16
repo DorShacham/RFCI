34a84b312567f57300029aa910bb34607641a520
Dor Shacham <dor.shacham@gmail.com>
2025-03-16 14:09:33 +0200

    .

--- ansatz_expressive_test.py
+++ ansatz_expressive_test.py
@@ -68,29 +68,8 @@ n = config['data']['n']
 p = config['data']['p']
 q = config['data']['q']
 saving_dir = str(f"results/vqe_simulation/jax/ansatz_expressive_test/Nx-{Nx}_Ny-{Ny}_p-{p}_q-{q}")
-layer_numer_range = range(2,4)
+layer_numer_range = range(1,4)
 
-
-overlap_array = []
-for layer_numer in layer_numer_range:
-    print(f"flux gate, layer number:{layer_numer}")
-
-    id = wandb.util.generate_id()
-    config['data']['id'] = id
-
-    wandb.init(
-        # set the wandb project where this run will be logged
-        project="RFCI-vqe",
-        name= f"Nx-{Nx}_Ny-{Ny}_p-{p}_q-{q}/expressive_test/{id}",
-        id = id,
-        # track hyperparameters and run metadata
-        config= config
-        )
-    saving_path = saving_dir +  str(f"/ln-{layer_numer}_flux")
-    config_list[0]['flux_gate_true'] = True
-    config_list[0]['layer_numer'] = layer_numer
-    res = jax_simulation.vqe_simulation(Nx = Nx, Ny = Ny, config_list = config_list, n = n, p=p, q=q, pre_ansatz = None,saveto = saving_path, log = True)
-    overlap_array.append(np.abs(res.fun))
 
 overlap_array_no_flux = []
 for layer_numer in layer_numer_range:
@@ -113,7 +92,26 @@ for layer_numer in layer_numer_range:
     res = jax_simulation.vqe_simulation(Nx = Nx, Ny = Ny, config_list = config_list, n = n, p=p, q=q, pre_ansatz = None,saveto = saving_path, log = True)
     overlap_array_no_flux.append(np.abs(res.fun))
 
+overlap_array = []
+for layer_numer in layer_numer_range:
+    print(f"flux gate, layer number:{layer_numer}")
 
+    id = wandb.util.generate_id()
+    config['data']['id'] = id
+
+    wandb.init(
+        # set the wandb project where this run will be logged
+        project="RFCI-vqe",
+        name= f"Nx-{Nx}_Ny-{Ny}_p-{p}_q-{q}/expressive_test/{id}",
+        id = id,
+        # track hyperparameters and run metadata
+        config= config
+        )
+    saving_path = saving_dir +  str(f"/ln-{layer_numer}_flux")
+    config_list[0]['flux_gate_true'] = True
+    config_list[0]['layer_numer'] = layer_numer
+    res = jax_simulation.vqe_simulation(Nx = Nx, Ny = Ny, config_list = config_list, n = n, p=p, q=q, pre_ansatz = None,saveto = saving_path, log = True)
+    overlap_array.append(np.abs(res.fun))
 
 
 
