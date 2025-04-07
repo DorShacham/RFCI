a08dbbf15658f241206914cccee4162e5ce92a27
Dor Shacham <dor.shacham@gmail.com>
2025-04-07 16:40:32 +0300

    .

--- condor_scripts/run_vqe.sh
+++ condor_scripts/run_vqe.sh
@@ -2,6 +2,7 @@
 
 source /Local/ph_lindner/anaconda3/bin/activate rfci-env
 
+ulimit -c unlimited
 # Run the Python script
 taskset -c $5 python ~/scripts/RFCI/RFCI/vqe_simulation.py --config_path configs/jax/Nx-$1_Ny-$2_layer-$3_flux-$4.yaml --log -cpu 1
 
