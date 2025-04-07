0b04dc91d2db4dd96dab0106f02d7eb69b86be4a
Dor Shacham <dor.shacham@gmail.com>
2025-04-07 16:48:44 +0300

    .

--- condor_scripts/run_vqe.sh
+++ condor_scripts/run_vqe.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 source /Local/ph_lindner/anaconda3/bin/activate rfci-env
-
 # Log compilation steps
 export JAX_LOG_COMPILES=1
 
@@ -16,7 +15,7 @@ export XLA_FLAGS=--xla_dump_to=/tmp/xla_dump
 
 ulimit -c unlimited
 # Run the Python script
-taskset -c $5  -X faulthandler python ~/scripts/RFCI/RFCI/vqe_simulation.py --config_path configs/jax/Nx-$1_Ny-$2_layer-$3_flux-$4.yaml --log -cpu 1
+taskset -c $5  python  -X faulthandler ~/scripts/RFCI/RFCI/vqe_simulation.py --config_path configs/jax/Nx-$1_Ny-$2_layer-$3_flux-$4.yaml --log -cpu 1
 
 conda deactivate
 
